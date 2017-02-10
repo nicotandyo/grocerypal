8895cc4e6815f82b48f617084e2bd7c89f3eb8db
Michael <lambm6@uw.edu>
2017-02-10 14:42:20 -0800

    scroll view for recipe results

--- app/src/main/java/group4/tcss450/uw/edu/grocerypal450/models/Recipe.java
+++ app/src/main/java/group4/tcss450/uw/edu/grocerypal450/models/Recipe.java
@@ -11,4 +11,41 @@ public class Recipe {
     public String mRecipeName;
     public String mRecipeId;
     public ArrayList<String> mIngredients;
+    public String mImage;
+
+    public Recipe () {
+
+    }
+
+    public String getRecipeName() {
+        return mRecipeName;
+    }
+
+    public String getRecipeId() {
+        return mRecipeId;
+    }
+
+    public ArrayList<String> getIngredients() {
+        return mIngredients;
+    }
+
+    public String getImgUrl() {
+        return mImage;
+    }
+
+    public void setRecipeName(String recipeName) {
+        mRecipeName = recipeName;
+    }
+
+    public void setRecipeId(String recipeId) {
+        mRecipeId = recipeId;
+    }
+
+    public void setIngredients(ArrayList<String> recipeIngredients) {
+        mIngredients = recipeIngredients;
+    }
+
+    public void setImage(String imageUrl) {
+        mImage = imageUrl;
+    }
 }
